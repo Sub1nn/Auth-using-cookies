d7f0cbeed2b80b751e30f5fcd927172489e9db93
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-06-28 13:04:17 +0300

    frontend part for cookie based authentication project completed

---
+++ frontend/src/components/User.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+
+interface UserData {
+  userId: number;
+  username: string;
+}
+
+export const User = () => {
+  const [userData, setUserData] = useState<UserData>();
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/user`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUserData(res.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      You're id is {userData?.userId}
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          axios.post(
+            `${BACKEND_URL}/logout`,
+            {},
+            {
+              withCredentials: true,
+            }
+          );
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
